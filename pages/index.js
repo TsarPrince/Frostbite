9bdd1cbb67fc6d2685d23aca86d511d55a630253
Prince Singh <marvelousprince012233@gmail.com>
2022-05-22 03:46:34 +0530

    integrated sanity with frontend

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,55 @@
 import React from 'react'
 
-const index = () => {
+import { client } from '../lib/client';
+import { urlFor } from '../lib/client';
+
+const index = ({ pets, students }) => {
+
   return (
-    <div>index</div>
+    <div>
+      {
+        pets.map(pet => {
+          return <div key={pet.slug.current} style={{'margin': '1rem', 'padding': '1rem', 'border':'2px solid black'}}> 
+            {
+              pet.image.map(petImg => <img key={petImg._key} src={urlFor(petImg)} width='100px'></img>)
+            }
+            <p>{pet.name}</p>
+            <p>{pet.gender}</p>
+            {pet.description.map(desc => <p key={desc}>{desc}</p>)}
+
+          </div>
+
+        })
+      }
+      {
+        students.map(student => {
+          return <div key={student.slug.current} style={{'margin': '1rem', 'padding': '1rem', 'border':'2px solid black'}}>
+            {
+              student.image.map(studentImg => <img key={studentImg._key} src={urlFor(studentImg)} width='100px'></img>)
+            }
+            <p>{student.name}</p>
+            <p>{student.gender}</p>
+            {student.description.map(desc => <p key={desc}>{desc}</p>)}
+
+          </div>
+
+        })
+      }
+    </div>
   )
+}
+
+// useEffect in Reactjs = getServerSideProps in Nextjs
+export const getServerSideProps = async () => {
+  const petQuery = '*[_type == "pet"]';
+  const pets = await client.fetch(petQuery);
+
+  const studentQuery = '*[_type == "student"]';
+  const students = await client.fetch(studentQuery);
+
+  return {
+    props: { pets, students }
+  }
 }
 
 export default index
