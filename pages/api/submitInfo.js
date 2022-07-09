3c990624260c816719c9b65821d649e326fc4916
Prince Singh <marvelousprince012233@gmail.com>
2022-07-09 20:52:52 +0530

    oki

--- pages/api/submitInfo.js
+++ pages/api/submitInfo.js
@@ -7,6 +7,6 @@ export default function handler(req, res) {
     console.log(req.body)
     res.status(200).json({ name: 'John Doe' })
   } else if (req.method === 'GET') {
-    res.status(200).json({'nikal laude': 'https://o3hzv34b.api.sanity.io/v2021-06-07/data/export/production/'})
+    res.status(200).json({'src': 'https://o3hzv34b.api.sanity.io/v2021-06-07/data/export/production/'})
   }
 }
