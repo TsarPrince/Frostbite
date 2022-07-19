695e84e0648e396302d5b4bb3be06ed52eed761c
Prince Singh <marvelousprince012233@gmail.com>
2022-07-19 15:45:19 +0530

    navbar fix 3

--- pages/login.js
+++ pages/login.js
@@ -7,7 +7,7 @@ const login = () => {
       <div className="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
         <div className="max-w-md w-full space-y-8">
           <div>
-            <img className="mx-auto h-12 w-auto" src="frostbite.png" alt="Workflow" />
+            <img className="mx-auto h-12 w-auto" src="/frostbite-dark.svg" alt="Workflow" />
             <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">Sign in to your account</h2>
             <p className="mt-2 text-center text-sm text-gray-600">
               Don&apos;t have one?&nbsp;
