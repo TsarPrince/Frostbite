60d9120a9e7e97cdc9c55cbc96633f87d1d7c1e8
Prince Singh <marvelousprince012233@gmail.com>
2022-07-20 02:03:35 +0530

    login signup connected to frontend

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,29 +1,27 @@
 import React, { useState } from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
+import { urlFor } from '../lib/client';
+
 
 export default function Navbar() {
   // const [clicked, setClicked] = React.useState(false)
-  const [styles, setStyles] = useState({ visibility: "hidden" })
   const [active, setActive] = useState('home')
   const activeLinkStyle = 'bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium cursor-pointer'
   const activeLinkStyleMobile = 'bg-gray-900 text-white block px-3 py-2 rounded-md text-base font-medium'
   const inactiveLinkStyle = 'text-gray-300 hover:bg-gray-800 hover:text-white px-3 py-2 rounded-md text-sm font-medium cursor-pointer'
   const inactiveLinkStyleMobile = 'text-gray-300 hover:bg-gray-800 hover:text-white block px-3 py-2 rounded-md text-base font-medium'
 
-  function focus() {
-    setStyles({ visibility: "visible" })
+  const toggleUserMenu = () => {
+    document.querySelector('#user-menu').classList.toggle('hidden');
   }
-  function blur() {
-    setStyles({ visibility: "hidden" })
-  }
-
   const toggleMobileMenu = () => {
     document.querySelector('#mobile-menu').classList.toggle('hidden');
     document.querySelector('#hamburger-icon').classList.toggle('hidden');
     document.querySelector('#close-icon').classList.toggle('hidden');
   }
 
+  if (typeof window == 'undefined') return <></>
 
   return (
     <nav className="bg-gray-900">
@@ -67,11 +65,6 @@ export default function Navbar() {
                     About us
                   </div>
                 </Link>
-                <Link href="/login">
-                  <div onClick={() => { setActive('login') }} className={`${(active == 'login') ? activeLinkStyle : inactiveLinkStyle}`}>
-                    Login
-                  </div>
-                </Link>
                 <Link href="/chat">
                   <div onClick={() => { setActive('chat') }} className={`${(active == 'chat') ? activeLinkStyle : inactiveLinkStyle}`}>
                     Chat
@@ -94,21 +87,45 @@ export default function Navbar() {
                 <svg className='h-4 w-4' xmlns="http://www.w3.org/2000/svg" fill='currentColor' viewBox="0 0 512 512"><path d="M500.3 443.7l-119.7-119.7c27.22-40.41 40.65-90.9 33.46-144.7C401.8 87.79 326.8 13.32 235.2 1.723C99.01-15.51-15.51 99.01 1.724 235.2c11.6 91.64 86.08 166.7 177.6 178.9c53.8 7.189 104.3-6.236 144.7-33.46l119.7 119.7c15.62 15.62 40.95 15.62 56.57 0C515.9 484.7 515.9 459.3 500.3 443.7zM79.1 208c0-70.58 57.42-128 128-128s128 57.42 128 128c0 70.58-57.42 128-128 128S79.1 278.6 79.1 208z" /></svg>
               </button>
             </Link>
-            {/* Profile Avatar */}
-            <div className="ml-3 relative">
-              <div onFocus={focus} onBlur={blur}>
-                <button type="button" className="bg-gray-800 flex text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white" aria-expanded="false" aria-haspopup="true">
-                  <span className="sr-only">Open user menu</span>
-                  <img className="h-8 w-8 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />
-                </button>
-              </div>
-              {/* Profile Menu */}
-              <div className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none transition ease-in-out duration-300" style={styles} role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabIndex="-1">
-                <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-200" role="menuitem" tabIndex="-1" id="user-menu-item-0">Your Profile</a>
-                <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-200" role="menuitem" tabIndex="-1" id="user-menu-item-1">Settings</a>
-                <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-200" role="menuitem" tabIndex="-1" id="user-menu-item-2">Sign out</a>
-              </div>
-            </div>
+
+            {
+              (localStorage.getItem('user'))
+                ? (
+                  <div>
+                    {/* Profile Avatar */}
+                    <div className="ml-3 relative">
+                      <div onClick={toggleUserMenu}>
+                        <button type="button" className="bg-gray-800 flex text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white" aria-expanded="false" aria-haspopup="true">
+                          <span className="sr-only">Open user menu</span>
+                          <img id='profile-avatar' className="h-8 w-8 rounded-full" src={urlFor(JSON.parse(localStorage.getItem('user')).image[0])} alt="profile avatar" />
+                        </button>
+                      </div>
+                      {/* User Menu */}
+                      <div id='user-menu' className="z-50 hidden origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none transition ease-in-out duration-300" role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabIndex="-1">
+                        <Link href={`/profile/${JSON.parse(localStorage.getItem('user')).rollno.toLowerCase()}`}><a onClick={toggleUserMenu} className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-200" role="menuitem">Your Profile</a></Link>
+                        <Link href='/login'><a onClick={() => {localStorage.removeItem('user'); toggleUserMenu();}} className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-200" role="menuitem">Sign out</a></Link>
+                      </div>
+                    </div>
+                  </div>
+                )
+                : (
+                  <div className='hidden lg:flex lg:items-center px-2'>
+                    <Link href="/login">
+                      <div onClick={() => { setActive('login') }} className={`${(active == 'login') ? activeLinkStyle : inactiveLinkStyle}`}>
+                        Login
+                      </div>
+                    </Link>
+                    {/* Divider */}
+                    <div className='w-px h-4 bg-white'></div>
+                    <Link href="/signup">
+                      <div onClick={() => { setActive('signup') }} className={`${(active == 'signup') ? activeLinkStyle : inactiveLinkStyle}`}>
+                        SignUp
+                      </div>
+                    </Link>
+                  </div>
+                )
+            }
+
           </div>
         </div>
       </div>
@@ -118,9 +135,10 @@ export default function Navbar() {
           <Link href='/'><a onClick={() => { setActive('home'); toggleMobileMenu() }} className={`${(active == 'home') ? activeLinkStyleMobile : inactiveLinkStyleMobile}`} aria-current="page">Home</a></Link>
           <Link href='/submitInfo'><a onClick={() => { setActive('submitInfo'); toggleMobileMenu() }} className={`${(active == 'submitInfo') ? activeLinkStyleMobile : inactiveLinkStyleMobile}`}>Submit Information</a></Link>
           <Link href='/aboutUs'><a onClick={() => { setActive('aboutUs'); toggleMobileMenu() }} className={`${(active == 'aboutUs') ? activeLinkStyleMobile : inactiveLinkStyleMobile}`}>About Us</a></Link>
-          <Link href='/login'><a onClick={() => { setActive('login'); toggleMobileMenu() }} className={`${(active == 'login') ? activeLinkStyleMobile : inactiveLinkStyleMobile}`}>Login</a></Link>
           <Link href='/chat'><a onClick={() => { setActive('chat'); toggleMobileMenu() }} className={`${(active == 'chat') ? activeLinkStyleMobile : inactiveLinkStyleMobile}`}>Chat</a></Link>
           <Link href='/search'><a onClick={() => { setActive('search'); toggleMobileMenu() }} className={`${(active == 'search') ? activeLinkStyleMobile : inactiveLinkStyleMobile}`}>Search</a></Link>
+          <Link href='/login'><a onClick={() => { setActive('login'); toggleMobileMenu() }} className={`${(active == 'login') ? activeLinkStyleMobile : inactiveLinkStyleMobile}`}>Login</a></Link>
+          <Link href='/signup'><a onClick={() => { setActive('signup'); toggleMobileMenu() }} className={`${(active == 'signup') ? activeLinkStyleMobile : inactiveLinkStyleMobile}`}>Sign Up</a></Link>
         </div>
       </div>
     </nav>
