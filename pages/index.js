3650b3a071189175a420c007e828423f859704ec
Prince Singh <marvelousprince012233@gmail.com>
2023-01-20 12:48:32 +0530

    vercel shat

--- pages/index.js
+++ pages/index.js
@@ -29,14 +29,14 @@ const index = () => {
       <Head>
         {/* <!-- Hotjar Tracking Code for https://frostbite-two.vercel.app/ --> */}
         <script>
-          {(function (h, o, t, j, a, r) {
+          {typeof window !== 'undefined' ? (function (h, o, t, j, a, r) {
             h.hj = h.hj || function () { (h.hj.q = h.hj.q || []).push(arguments) };
             h._hjSettings = { hjid: 3330601, hjsv: 6 };
             a = o.getElementsByTagName('head')[0];
             r = o.createElement('script'); r.async = 1;
             r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv;
             a.appendChild(r);
-          })(window, document, 'https://static.hotjar.com/c/hotjar-', '.js?sv=')}
+          })(window, document, 'https://static.hotjar.com/c/hotjar-', '.js?sv=') : (function () { })()}
         </script>
       </Head>
       <Hero />
